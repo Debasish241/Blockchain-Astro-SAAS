b2afe016186576017879f254482de2752cf87600
DebasishDebnath <debasish.debnath@iemlabs.com>
2024-10-04 17:56:44 +0530

    Feature section, Homepage cards done

---
+++ src/sections/FeaturesCards.tsx
@@ -0,0 +1,123 @@
+import { twMerge } from "tailwind-merge";
+import { TextButton } from "../components/TextButton";
+
+const cardData = [
+  {
+    image: "/assets/images/pill.png",
+    title: "   Revolutionary Blockchain API",
+    description:
+      "Effortlessly integrate and manage blockchain data with our cutting-edge API, designed for seamless connectivity",
+    color: "fuchsia",
+  },
+  {
+    image: "/assets/images/cuboid.png",
+    title: "Decentralized Data Solutions",
+    description:
+      "Empower your applications with decentralized data solutions,ensuring security and transparency at every step",
+    color: "lime",
+  },
+  {
+    image: "/assets/images/cone.png",
+    title: "Next-Gen Smart Contracts",
+    description:
+      "Unlock the potential of next-gen smart contracts with our robust and scalable API, tailored for modern blockchain needs.",
+    color: "cyan",
+  },
+  {
+    image: "/assets/images/icosahedron.png",
+    title: "Seamless Blockchain Integration",
+    description:
+      "Integrate blockchain technology seamlessly into your projects, with minimal effort and maximum efficiency.",
+    color: "violet",
+  },
+];
+
+export const FeatureCardsSection = () => {
+  return (
+    <section className="py-24 overflow-x-clip md:-mt-28">
+      <div className="container">
+        <h2 className="font-heading font-black text-4xl text-center md:text-5xl lg:text-6xl">
+          Discover the future of blockchain with Blockforge.
+        </h2>
+
+        <div className="mt-36 flex lg:mt-48 ">
+          <div className="flex-none flex gap-8">
+            {cardData.map(({ image, title, description, color }) => (
+              <div
+                className="relative z-0 p-8 md:p-10 max-w-xs group md:max-w-md "
+                key={title}
+              >
+                {/* <div className="absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300"></div> */}
+                <div
+                  className={twMerge(
+                    "absolute size-16 rounded-xl bg-fuchsia-500 group-hover:bg-fuchsia-400 transition duration-300 top-1.5 right-1.5 -z-10",
+                    color === "lime" && "bg-lime-500 group-hover:bg-lime-400",
+                    color === "cyan" && "bg-cyan-500 group-hover:bg-cyan-400",
+                    color === "violet" &&
+                      "bg-violet-500 group-hover:bg-violet-400"
+                  )}
+                ></div>
+
+                <div className="absolute inset-0 -z-10 bg-zinc-800 rounded-2xl [mask-image:linear-gradient(225deg, transparent, transparent_40px, black_40px)]"></div>
+                <div className="flex justify-center -mt-28 ">
+                  <div className="inline-flex relative">
+                    {/* <div className="absolute h-4"></div> */}
+                    <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
+                    <img
+                      src={image}
+                      alt="pill"
+                      className="size-40 group-hover:-translate-y-6 transition duration-300"
+                    />
+                  </div>
+                </div>
+                <h3 className="font-heading font-black text-3xl mt-12">
+                  {title}
+                </h3>
+                <p className="text-lg text-zinc-400 mt-4">{description}</p>
+                <div className="flex justify-between mt-12 ">
+                  <TextButton color={color}>Learn More</TextButton>
+                  {/* <button
+                    className={twMerge(
+                      "text-sm font-heading uppercase font-extrabold tracking-wider text-fuchsia-500",
+                      color === "lime" && "text-lime-500",
+                      color === "cyan" && "text-cyan-500",
+                      color === "violet" && "text-violet-500"
+                    )}
+                  >
+                    Learn More
+                  </button> */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-8 text-zinc-500 group-hover:text-zinc-300 transition duration-300 -translate-x-2 group-hover:translate-x-0"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                    />
+                  </svg>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-center mt-10">
+          <div className="bg-zinc-950 inline-flex gap-4 p-2.5 rounded-full ">
+            {[
+              cardData.map(({ title }) => (
+                <div
+                  key={title}
+                  className="size-2.5 bg-zinc-500 rounded-full cursor-pointer"
+                ></div>
+              )),
+            ]}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/sections/FeaturesGrid.tsx
@@ -0,0 +1,49 @@
+import { CutCornerButton } from "../components/CutSornerButton";
+import { TextButton } from "../components/TextButton";
+const listItems = [
+  "Experience unparalleled security and scalibility",
+  "Fully benefit from scalable network effects",
+  "Unlock the potential off decentralized networks",
+];
+
+export const FeaturesGrid = () => {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div>
+          <h2 className="font-heading font-black text-4xl">
+            Empowering the future of blockchain
+          </h2>
+          <p className="text-xl text-zinc-400 mt-8">
+            Blockchain provides robust and secure infrastructure to support the
+            nexxt generation of decentralized application
+          </p>
+
+          <ul className="flex flex-col gap-8 mt-12 ">
+            {listItems.map((item) => (
+              <li key={item} className="flex items-center gap-3">
+                <div className="inline-flex flex-shrink-0 justify-center items-center size-8 outline outline-4 -outline-offset-4 rounded-full outline-fuchsia-500/10">
+                  <div className="size-1.5 bg-fuchsia-500 rounded-full "></div>
+                </div>
+                <span className="text-xl font-bold ">{item}</span>
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-8 mt-12 ">
+            <CutCornerButton>Get Started</CutCornerButton>
+            <TextButton>Learn More</TextButton>
+          </div>
+        </div>
+        <div>
+          <h2>Blockforge leads the way.</h2>
+          <p>
+            Blockforge is dedicated to supporting the evolution of web3
+            applications by delivering the necessary infrastructure and security
+            for web3.
+          </p>
+          <p></p>
+        </div>
+      </div>
+    </section>
+  );
+};
